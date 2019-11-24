c083158ae0df8b73cf65659f17b89cb2500312ac
maruchin <modelarstwo1>
2019-11-24 13:23:48 +0100

    Ona dialogowe - pełne otworzenie okna, nawet jeśli większe niż połowa ekranu

--- app/src/main/java/com/example/medihelper/presentation/framework/AppBottomSheetDialog.kt
+++ app/src/main/java/com/example/medihelper/presentation/framework/AppBottomSheetDialog.kt
@@ -1,8 +1,16 @@
 package com.example.medihelper.presentation.framework
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.MutableLiveData
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import android.widget.FrameLayout
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import android.view.ViewTreeObserver
+
+
 
 abstract class AppBottomSheetDialog : BottomSheetDialogFragment() {
     abstract val TAG: String
@@ -12,5 +20,18 @@ abstract class AppBottomSheetDialog : BottomSheetDialogFragment() {
 
     fun setColorPrimary(colorResId: Int) {
         colorPrimaryLive.value = colorResId
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                view.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                val dialog = dialog as BottomSheetDialog?
+                val bottomSheet = dialog!!.findViewById(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout?
+                val behavior = BottomSheetBehavior.from(bottomSheet!!)
+                behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            }
+        })
     }
 }
