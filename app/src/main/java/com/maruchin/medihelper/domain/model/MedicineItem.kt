49888d6299966443074cff6587defead209d6164
maruchin <modelarstwo1>
2019-12-07 13:42:08 +0100

    Pobieranie i wyświetlanie zdjęć leków na liście i w szczegółach

--- app/src/main/java/com/maruchin/medihelper/domain/model/MedicineItem.kt
+++ app/src/main/java/com/maruchin/medihelper/domain/model/MedicineItem.kt
@@ -2,17 +2,20 @@ package com.maruchin.medihelper.domain.model
 
 import com.maruchin.medihelper.domain.entities.Medicine
 import com.maruchin.medihelper.domain.entities.MedicineStateData
+import java.io.File
 
 data class MedicineItem(
     val medicineId: String,
     val name: String,
     val unit: String,
-    val stateData: MedicineStateData
+    val stateData: MedicineStateData,
+    val pictureFile: File?
 ) {
-    constructor(medicine: Medicine) : this(
+    constructor(medicine: Medicine, pictureFile: File?) : this(
         medicineId = medicine.medicineId,
         name = medicine.name,
         unit = medicine.unit,
-        stateData = medicine.getStateData()
+        stateData = medicine.getStateData(),
+        pictureFile = pictureFile
     )
 }
--- app/src/main/java/com/maruchin/medihelper/domain/usecases/medicines/GetAllMedicinesItemsLiveUseCase.kt
+++ app/src/main/java/com/maruchin/medihelper/domain/usecases/medicines/GetAllMedicinesItemsLiveUseCase.kt
@@ -1,7 +1,9 @@
 package com.maruchin.medihelper.domain.usecases.medicines
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
+import androidx.lifecycle.liveData
 import com.maruchin.medihelper.domain.entities.Medicine
 import com.maruchin.medihelper.domain.entities.MedicineStateData
 import com.maruchin.medihelper.domain.model.MedicineItem
@@ -10,11 +12,25 @@ import com.maruchin.medihelper.domain.repositories.MedicineRepo
 class GetAllMedicinesItemsLiveUseCase(
     private val medicineRepo: MedicineRepo
 ) {
-
     suspend fun execute(): LiveData<List<MedicineItem>> {
         val allEntitiesLive = medicineRepo.getAllListLive()
-        return Transformations.map(allEntitiesLive) { list ->
-            list.map { MedicineItem(it) }
+        return Transformations.switchMap(allEntitiesLive) { list ->
+            liveData {
+                val itemsList = mapToMedicineItems(list)
+                emit(itemsList)
+            }
         }
+    }
+
+    private suspend fun mapToMedicineItems(medicinesList: List<Medicine>): List<MedicineItem> {
+        val itemsList = mutableListOf<MedicineItem>()
+        medicinesList.forEach { medicine ->
+            val pictureFile = medicine.pictureName?.let {
+                medicineRepo.getMedicinePicture(it)
+            }
+            val medicineItem = MedicineItem(medicine, pictureFile)
+            itemsList.add(medicineItem)
+        }
+        return itemsList
     }
 }
