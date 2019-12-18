d6dce6391e84d2ad1edccd3bce2252a33498cc17
maruchin <modelarstwo1>
2019-12-19 00:56:39 +0100

    Szczegóły planu - dni przyjmowania

--- app/src/main/java/com/maruchin/medihelper/presentation/feature/mediplan/MedicinePlanDetailViewModel.kt
+++ app/src/main/java/com/maruchin/medihelper/presentation/feature/mediplan/MedicinePlanDetailViewModel.kt
@@ -1,12 +1,13 @@
 package com.maruchin.medihelper.presentation.feature.mediplan
 
 import androidx.lifecycle.*
-import com.maruchin.medihelper.domain.entities.AppDate
+import com.maruchin.medihelper.domain.entities.IntakeDays
 import com.maruchin.medihelper.domain.entities.MedicinePlan
 import com.maruchin.medihelper.domain.model.MedicinePlanDetails
 import com.maruchin.medihelper.domain.usecases.mediplans.GetMedicinePlanDetailsUseCase
 import com.maruchin.medihelper.presentation.framework.ActionLiveData
 import kotlinx.coroutines.launch
+import java.lang.StringBuilder
 
 class MedicinePlanDetailViewModel(
     private val getMedicinePlanDetailsUseCase: GetMedicinePlanDetailsUseCase
@@ -16,6 +17,7 @@ class MedicinePlanDetailViewModel(
     val medicineName: LiveData<String>
     val medicineUnit: LiveData<String>
     val durationTime: LiveData<DurationTime>
+    val days: LiveData<Days>
 
     val actionDataLoaded: LiveData<Boolean>
         get() = _actionDataLoaded
@@ -30,25 +32,8 @@ class MedicinePlanDetailViewModel(
         colorPrimary = Transformations.map(medicinePlanDetails) { it.profileColor }
         medicineName = Transformations.map(medicinePlanDetails) { it.medicineName }
         medicineUnit = Transformations.map(medicinePlanDetails) { it.medicineUnit }
-        durationTime = Transformations.map(medicinePlanDetails) {
-            DurationTime(
-                planType = when (it.planType) {
-                    MedicinePlan.Type.ONCE -> "Jednorazowo"
-                    MedicinePlan.Type.PERIOD -> "Przez okres dni"
-                    MedicinePlan.Type.CONTINUOUS -> "Przyjmowanie ciągłe"
-                },
-                startDate = if (it.endDate != null) {
-                    "Od ${it.startDate.formatString}"
-                } else {
-                    it.startDate.formatString
-                },
-                endDate = if (it.endDate != null) {
-                    "Do ${it.endDate.formatString}"
-                } else {
-                    null
-                }
-            )
-        }
+        durationTime = Transformations.map(medicinePlanDetails) { getDurationTime(it) }
+        days = Transformations.map(medicinePlanDetails) { details -> details.intakeDays?.let { getDays(it) } }
     }
 
     fun setArgs(args: MedicinePlanDetailsFragmentArgs) = viewModelScope.launch {
@@ -59,9 +44,70 @@ class MedicinePlanDetailViewModel(
         _actionDataLoaded.sendAction()
     }
 
+    private fun getDurationTime(details: MedicinePlanDetails): DurationTime {
+        return DurationTime(
+            planType = when (details.planType) {
+                MedicinePlan.Type.ONCE -> "Jednorazowo"
+                MedicinePlan.Type.PERIOD -> "Przez okres dni"
+                MedicinePlan.Type.CONTINUOUS -> "Przyjmowanie ciągłe"
+            },
+            startDate = if (details.endDate != null) {
+                "Od ${details.startDate.formatString}"
+            } else {
+                details.startDate.formatString
+            },
+            endDate = if (details.endDate != null) {
+                "Do ${details.endDate.formatString}"
+            } else {
+                null
+            }
+        )
+    }
+
+    private fun getDays(intakeDays: IntakeDays): Days {
+        var daysType = ""
+        var daysDetails: String? = null
+        when (intakeDays) {
+            is IntakeDays.Everyday -> {
+                daysType = "Codziennie"
+            }
+            is IntakeDays.DaysOfWeek -> {
+                daysType = "Wybrane dni tygodnia"
+                daysDetails = daysOfWeekToString(intakeDays)
+            }
+            is IntakeDays.Interval -> {
+                daysType = "Co ${intakeDays.daysCount} dni"
+            }
+            is IntakeDays.Sequence -> {
+                daysType = "Sekwencja dni"
+                daysDetails = "${intakeDays.intakeCount} dni przyjmowania, ${intakeDays.notIntakeCount} dni przerwy"
+            }
+        }
+        return Days(daysType, daysDetails)
+    }
+
+    private fun daysOfWeekToString(daysOfWeek: IntakeDays.DaysOfWeek): String {
+        return StringBuilder()
+            .append(if (daysOfWeek.monday) "po, " else "")
+            .append(if (daysOfWeek.tuesday) "wt, " else "")
+            .append(if (daysOfWeek.wednesday) "śr, " else "")
+            .append(if (daysOfWeek.thursday) "cz, " else "")
+            .append(if (daysOfWeek.friday) "pi, " else "")
+            .append(if (daysOfWeek.saturday) "so, " else "")
+            .append(if (daysOfWeek.sunday) "nd, " else "")
+            .toString().apply {
+                dropLast(2)
+            }
+    }
+
     data class DurationTime(
         val planType: String,
         val startDate: String,
         val endDate: String?
+    )
+
+    data class Days(
+        val daysType: String,
+        val daysDetails: String?
     )
 }
