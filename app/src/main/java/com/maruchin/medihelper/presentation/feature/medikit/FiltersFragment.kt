2c4e7f640d083540ebdd3961d42fb26fb707f126
Maruchin1 <HakontoMotonto1>
2020-01-04 20:06:30 +0100

    Implementacja widoku filtrów w apteczce

---
+++ app/src/main/java/com/maruchin/medihelper/presentation/feature/medikit/FiltersFragment.kt
@@ -0,0 +1,10 @@
+package com.maruchin.medihelper.presentation.feature.medikit
+
+import com.maruchin.medihelper.R
+import com.maruchin.medihelper.databinding.FragmentFiltersBinding
+import com.maruchin.medihelper.presentation.framework.BaseMainFragment
+
+class FiltersFragment : BaseMainFragment<FragmentFiltersBinding>(R.layout.fragment_filters) {
+
+
+}
