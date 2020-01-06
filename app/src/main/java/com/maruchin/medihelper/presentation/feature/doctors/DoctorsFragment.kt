742499a9f93475ee801e96a7e1b1323ce5330366
Maruchin1 <HakontoMotonto1>
2020-01-06 13:07:05 +0100

    Pusta zakładka lekarz z informacją o przyszłych funkcjonalnościach

---
+++ app/src/main/java/com/maruchin/medihelper/presentation/feature/doctors/DoctorsFragment.kt
@@ -0,0 +1,7 @@
+package com.maruchin.medihelper.presentation.feature.doctors
+
+import com.maruchin.medihelper.R
+import com.maruchin.medihelper.databinding.FragmentDoctorsBinding
+import com.maruchin.medihelper.presentation.framework.BaseHomeFragment
+
+class DoctorsFragment : BaseHomeFragment<FragmentDoctorsBinding>(R.layout.fragment_doctors)
